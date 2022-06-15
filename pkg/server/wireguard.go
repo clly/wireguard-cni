0a7adcac513747499f7cdfa44f01b7a85ac944cb
Connor Kelly <connor.r.kelly@gmail.com>
2022-06-14 23:21:43 -0500

    Implement wireguard Peers RPC
    * Add buf.yaml for lint rules
    * Add Route to Peer Message
    * Move expvar.Publish to init()
    * Add List() to mapDB

--- pkg/server/wireguard.go
+++ pkg/server/wireguard.go
@@ -42,7 +42,7 @@ func (s *Server) Register(ctx context.Context,
 func (s *Server) registerWGKey(pk string, msg *wireguardv1.RegisterRequest) error {
 	b, err := protojson.Marshal(msg)
 	if err != nil {
-		return fmt.Errorf("proto fuckup %w", err)
+		return fmt.Errorf("proto is broke %w", err)
 	}
 	s.wgKey.Set(pk, string(b))
 	s.expvarMap.Set(pk, msg)
@@ -52,7 +52,35 @@ func (s *Server) registerWGKey(pk string, msg *wireguardv1.RegisterRequest) erro
 func (s *Server) Peers(ctx context.Context,
 	req *connect.Request[wireguardv1.PeersRequest],
 ) (*connect.Response[wireguardv1.PeersResponse], error) {
-	p := &wireguardv1.PeersResponse{}
+
+	keyList := s.wgKey.List()
+	peers := make([]*wireguardv1.Peer, 0, len(keyList))
+	for _, v := range keyList {
+		regReq, err := registerFromString(v)
+		if err != nil {
+			return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to marshal register request %w", err))
+		}
+		p := &wireguardv1.Peer{
+			PublicKey: regReq.GetPublicKey(),
+			Endpoint:  regReq.GetEndpoint(),
+			Route:     regReq.GetRoute(),
+		}
+		peers = append(peers, p)
+	}
+
+	p := &wireguardv1.PeersResponse{
+		Peers: peers,
+	}
 
 	return connect.NewResponse(p), nil
+}
+
+func registerFromString(s string) (*wireguardv1.RegisterRequest, error) {
+	p := &wireguardv1.RegisterRequest{}
+	err := protojson.Unmarshal([]byte(s), p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, err
 }
--- pkg/server/wireguard_test.go
+++ pkg/server/wireguard_test.go
@@ -7,39 +7,10 @@ import (
 	wireguardv1 "wireguard-cni/gen/wgcni/wireguard/v1"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/hashicorp/go-uuid"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/encoding/protojson"
 )
-
-func Test_Peers(t *testing.T) {
-	testcases := []struct {
-		name string
-		req  *wireguardv1.PeersRequest
-		resp *wireguardv1.PeersResponse
-		err  error
-	}{
-		{
-			name: "HappyPath",
-			req:  &wireguardv1.PeersRequest{},
-			resp: &wireguardv1.PeersResponse{},
-		},
-	}
-	for _, testcase := range testcases {
-		t.Run(testcase.name, func(t *testing.T) {
-			r := require.New(t)
-			s := NewServer()
-			expectedResponse := connect.NewResponse(testcase.resp)
-			req := connect.NewRequest(testcase.req)
-			resp, err := s.Peers(context.Background(), req)
-			if testcase.err != nil {
-				r.Error(err)
-				r.EqualError(testcase.err, err.Error())
-			} else {
-				r.Nil(err)
-				r.Equal(expectedResponse, resp)
-			}
-		})
-	}
-}
 
 func Test_Register(t *testing.T) {
 	testcases := []struct {
@@ -104,4 +75,80 @@ func Test_Register(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_Peers(t *testing.T) {
+	testcases := []struct {
+		name      string
+		peersFunc func(t *testing.T, m *mapDB)
+		err       error
+	}{
+		{
+			name: "HappyPathEmpty",
+		},
+		{
+			name: "HappyPathWithPeers",
+			peersFunc: func(t *testing.T, m *mapDB) {
+				r := validRegisterReq(t)
+				b, err := protojson.Marshal(r)
+				require.NoError(t, err)
+				m.Set(r.PublicKey, string(b))
+			},
+		},
+		{
+			name: "BadValuesInMapDB",
+			peersFunc: func(t *testing.T, m *mapDB) {
+				m.Set("helo", "will-not-marshal")
+			},
+			err: connect.NewError(connect.CodeInternal, errors.New("failed to marshal register request proto: syntax error (line 1:1): invalid value will-not-marshal")),
+		},
+	}
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			r := require.New(t)
+			s := NewServer()
+			m := newMapDB()
+			if testcase.peersFunc != nil {
+				testcase.peersFunc(t, m)
+				reqs := m.List()
+				for _, reqS := range reqs {
+					req, err := registerFromString(reqS)
+					if err == nil {
+						s.registerWGKey(req.PublicKey, req)
+					} else {
+						s.wgKey.Set("helo", "will-not-marshal")
+					}
+				}
+			}
+
+			req := connect.NewRequest(&wireguardv1.PeersRequest{})
+			resp, err := s.Peers(context.Background(), req)
+			if testcase.err != nil {
+				r.Error(err)
+				r.EqualError(testcase.err, err.Error())
+			} else {
+				r.NoError(err)
+				for _, peer := range resp.Msg.GetPeers() {
+					v, ok := m.Get(peer.PublicKey)
+					r.True(ok)
+					req, err := registerFromString(v)
+					r.NoError(err)
+					r.Equal(req.Endpoint, peer.Endpoint)
+					r.Equal(req.PublicKey, peer.PublicKey)
+					r.Equal(req.Route, peer.Route)
+				}
+			}
+		})
+	}
+}
+
+func validRegisterReq(t *testing.T) *wireguardv1.RegisterRequest {
+	u, err := uuid.GenerateUUID()
+	require.NoError(t, err)
+	r := &wireguardv1.RegisterRequest{
+		PublicKey: u,
+		Endpoint:  "192.168.1.1:51820",
+		Route:     "10.0.0.0/24",
+	}
+	return r
 }
