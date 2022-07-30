14a4d2937981a84cd224d986d50465365d94a5cd
Connor Kelly <connor.r.kelly@gmail.com>
2022-07-30 13:40:06 -0500

    Listen on 0.0.0.0

--- cmd/cluster-manager/main.go
+++ cmd/cluster-manager/main.go
@@ -43,8 +43,8 @@ func main() {
 	mux.Handle(path, handler)
 
 	mux.Handle("/debug/varz", expvar.Handler())
-	log.Println("listening localhost:8080 ...")
-	log.Fatal(http.ListenAndServe("localhost:8080", h2c.NewHandler(mux, &http2.Server{})))
+	log.Println("listening :8080 ...")
+	log.Fatal(http.ListenAndServe(":8080", h2c.NewHandler(mux, &http2.Server{})))
 }
 
 func config() ClusterManagerConfig {
