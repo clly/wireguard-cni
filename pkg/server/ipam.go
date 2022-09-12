7061b7d42785c14801e3fe46ca27ac380daa8916
Connor Kelly <connor.r.kelly@gmail.com>
2022-09-11 21:16:54 -0500

    Create wrapper around ipam
    New wrapper around the go-ipam object to more easily save and load the
    data file.

--- pkg/server/ipam.go
+++ pkg/server/ipam.go
@@ -3,8 +3,12 @@ package server
 import (
 	"context"
 	"expvar"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"sync"
 
 	"github.com/bufbuild/connect-go"
@@ -19,6 +23,84 @@ var (
 	wireguardExpvar                                  = new(expvar.Map).Init()
 	once                                             = &sync.Once{}
 )
+
+// ipam is a wrapper around github.com/metal-stack/go-ipam.Ipamer to make persisting and loading the ipam state easier
+type ipam struct {
+	goipam.Ipamer
+	persistFile string
+}
+
+const IpamDataFile = "ipam.json"
+
+func newIPAM(ctx context.Context, dataDir, cidr string) (*ipam, error) {
+	ipamer := goipam.New()
+
+	_, err := ipamer.NewPrefix(ctx, cidr)
+	if err != nil {
+		return nil, err
+	}
+	var persistFile string
+	if dataDir != "" {
+		persistFile = filepath.Join(dataDir, IpamDataFile)
+	}
+	ipam := &ipam{
+		persistFile: persistFile,
+		Ipamer:      ipamer,
+	}
+	if err := ipam.loadData(ctx); err != nil {
+		return nil, err
+	}
+	return ipam, nil
+}
+
+// save will dump ipam state from memory into a file
+func (i *ipam) save(ctx context.Context) error {
+	if i.persistFile == "" {
+		return nil
+	}
+	data, err := i.Ipamer.Dump(ctx)
+	if err != nil {
+		return err
+	}
+	f, err := os.OpenFile(i.persistFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC|os.O_SYNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(data)
+	return err
+}
+
+// loadData will load ipam state from the data directory
+func (i *ipam) loadData(ctx context.Context) error {
+	if _, err := os.Stat(i.persistFile); os.IsNotExist(err) {
+		return nil
+	}
+
+	err := i.deleteAllPrefixes(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to delete prefixes for loading %w", err)
+	}
+	b, err := ioutil.ReadFile(i.persistFile)
+	if err != nil {
+		return err
+	}
+	return i.Ipamer.Load(ctx, string(b))
+}
+
+func (i *ipam) deleteAllPrefixes(ctx context.Context) error {
+	prefixes, err := i.Ipamer.ReadAllPrefixCidrs(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to read prefixes for deletion %w", err)
+	}
+	for _, prefix := range prefixes {
+		_, err := i.Ipamer.DeletePrefix(ctx, prefix)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 type IPAM_MODE int
 
--- pkg/server/ipam_test.go
+++ pkg/server/ipam_test.go
@@ -2,9 +2,14 @@ package server
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
 	"testing"
 
 	"github.com/bufbuild/connect-go"
+	goipam "github.com/metal-stack/go-ipam"
 	"github.com/stretchr/testify/require"
 
 	ipamv1 "github.com/clly/wireguard-cni/gen/wgcni/ipam/v1"
@@ -47,5 +52,55 @@ func Test_Alloc(t *testing.T) {
 
 		})
 	}
+}
 
+func Test_NewIPAM(t *testing.T) {
+	const parentPrefix = "10.0.0.0/8"
+	testcases := map[string]struct {
+		withDataDir bool
+		prefixs     []string
+	}{
+		"EmptyDataDir": {
+			withDataDir: false,
+		},
+		"WithDataDir": {
+			withDataDir: true,
+		},
+		"WithStoredAllocs": {
+			withDataDir: true,
+			prefixs:     []string{"10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"},
+		},
+	}
+	for name, testcase := range testcases {
+		t.Run(name, func(t *testing.T) {
+			r := require.New(t)
+			ctx := context.Background()
+			d := t.TempDir()
+			ipam, err := newIPAM(context.Background(), d, parentPrefix)
+			r.NoError(err)
+			for _, s := range testcase.prefixs {
+				_, err := ipam.AcquireSpecificChildPrefix(ctx, parentPrefix, s)
+				r.NoError(err)
+			}
+			r.NoError(ipam.save(ctx))
+			ipam, err = newIPAM(context.Background(), d, "10.0.0.0/8")
+			r.NoError(err)
+			r.NoError(ipam.save(ctx))
+			if !testcase.withDataDir {
+				r.NoFileExists(IpamDataFile)
+			} else {
+				r.FileExists(filepath.Join(d, IpamDataFile))
+			}
+
+			if testcase.prefixs != nil {
+				actual, err := ipam.ReadAllPrefixCidrs(ctx)
+				r.NoError(err)
+				sort.Strings(actual)
+				expectedPrefixes := append(testcase.prefixs, parentPrefix)
+				sort.Strings(expectedPrefixes)
+				r.EqualValues(expectedPrefixes, actual)
+			}
+
+		})
+	}
 }
